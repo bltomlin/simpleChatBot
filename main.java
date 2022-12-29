d3a95d535606f25101044b116d69f186db99203e
bltomlin <62484274+bltomlin@users.noreply.github.com>
2022-12-28 21:32:57 -0800

    requests user input

--- main.java
+++ main.java
@@ -1,7 +1,18 @@
 package bot;
 
+import java.util.Scanner;
+
 public class SimpleBot {
     public static void main(String[] args) {
-        System.out.println("Hello! My name is Bot.\nI was created in 2022.");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2018.");
+        System.out.println("Please, remind me your name.");
+
+        // reading a name
+        String name = scanner.next();
+
+        System.out.println("What a great name you have, " + name + "!");
     }
 }
